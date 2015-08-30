af71a3301b4c997291e60c23b6141bc97ea2da01
Binh Tran <btran@jive.com>
2015-08-30 15:29:03 -0600

    Add scorpion module

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # senior-sky
 
 [![Build Status](https://travis-ci.org/ankel/senior-sky.svg?branch=master)](https://travis-ci.org/ankel/senior-sky)
+
+## Usage:
+````
+mvn clean install
+cd raptor
+mvn exec:java
+```
+then open `scorpion/index.html` in your browser of choice. Tested on Chrome 43.0.2357.134 (64-bit)
+and Safari 8.0.8 (10600.8.9)
---
+++ scorpion/js/page.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  var lightPath = 'http://localhost:8080/light';
+  var lightStatus = false;
+
+  var booleanToOnOff = function(boolean) {
+    return boolean ? 'on' : 'off';
+  }
+
+  var generateStatusText = function(boolean) {
+    return 'Your house\'s light is currently ' + booleanToOnOff(boolean);
+  }
+
+  var generateControlText = function(boolean) {
+      return 'Turn it ' + booleanToOnOff(!boolean);
+  }
+
+  var handleError = function(error) {
+    if (error)
+    {
+      $('#light').text('Failed to connect to our server. Please contact supports');
+      $('#control').toggleClass('none-visible', true);
+    } else {
+      $('#control').toggleClass('none-visible', false);
+    }
+  }
+
+  var modifyLightAndControlText = function(data) {
+    lightStatus = data.results.state;
+    $('#light').text(generateStatusText(lightStatus));
+    $('#control').text(generateControlText(lightStatus));
+  }
+
+  $.ajax({
+    url: lightPath,
+  })
+  .done(function(data) {
+    handleError(false);
+    modifyLightAndControlText(data);
+  })
+  .fail(function() {
+    handleError(true);
+  });
+
+  $('#control').click(function() {
+    $.ajax({
+      url: lightPath,
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(!lightStatus)
+    })
+    .done(function(data) {
+      handleError(false);
+      modifyLightAndControlText(data);
+    })
+    .fail(function() {
+      handleError(true);
+    })
+  });
+
+
+});
